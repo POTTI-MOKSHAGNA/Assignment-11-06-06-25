159228e34557ee01728ee1e8d93b69f3b7b59d6c
POTTI-MOKSHAGNA <150547653+POTTI-MOKSHAGNA@users.noreply.github.com>
2025-06-07 14:46:27 +0530

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,7 +1,7 @@
-function reverseString(){
+function sortString(){
     const input = document.getElementById('string')
     const span = document.getElementById('getValue')
     let inputValue = input.value
-    let result = inputValue.split("").reverse().join("")            
+    let result = inputValue.split("").sort().join("")            
     span.innerText = result
 }
